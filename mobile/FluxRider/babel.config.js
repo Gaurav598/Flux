c934ae1b27cb85c70c43ec37701f0f63a4d7d258
Gaurav598 <gr598895@gmail.com>
2026-09-13 00:33:34 +0530

    fix: change dotenv path and allowUndefined to fix babel crash

--- mobile/FluxRider/babel.config.js
+++ mobile/FluxRider/babel.config.js
@@ -6,9 +6,9 @@ module.exports = {
       'module:react-native-dotenv',
       {
         moduleName: '@env',
-        path: '../../.env',
+        path: '.env',
         safe: false,
-        allowUndefined: false,
+        allowUndefined: true,
       },
     ],
   ],
